220622506319ac2260a552f74baee32d00eac56a
BubbleFish <daforsastudia@gmail.com>
2025-03-06 12:09:34 +0300

    fix: Remove unused Parameters.root

--- mend/parameters.go
+++ mend/parameters.go
@@ -2,13 +2,11 @@ package mend
 
 import (
 	"fmt"
-	"strings"
 
 	"github.com/tidwall/gjson"
 )
 
 type Parameters struct {
-	root string
 	json string
 }
 
@@ -21,14 +19,9 @@ func NewParameters(json string) (*Parameters, error) {
 }
 
 func (params *Parameters) Get(path string) gjson.Result {
-	return gjson.Get(strings.Join([]string{params.root, params.json}, "."), path)
+	return gjson.Get(params.json, path)
 }
 
 func (params *Parameters) GetMany(paths ...string) []gjson.Result {
-	return gjson.GetMany(strings.Join([]string{params.root, params.json}, "."), paths...)
-}
-
-func (params *Parameters) SetRoot(root string) *Parameters {
-	params.root = root
-	return params
+	return gjson.GetMany(params.json, paths...)
 }
